b990952bd1593c1aa2038da623308c4631a8f0e7
Alexaderrand101git <Sonicrider2@yandex.ru>
2018-11-22 22:04:08 +0400

    пока вот

--- InterfaceStuff/Form1.Designer.cs
+++ InterfaceStuff/Form1.Designer.cs
@@ -30,23 +30,42 @@
         {
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.panel4 = new System.Windows.Forms.Panel();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.inputEcho = new System.Windows.Forms.RichTextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.rawInput = new System.Windows.Forms.TextBox();
             this.outputEcho = new System.Windows.Forms.RichTextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.baudRate = new System.Windows.Forms.ComboBox();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.portName = new System.Windows.Forms.ComboBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
-            this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.panel7.SuspendLayout();
+            this.panel6.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
             // 
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(937, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(726, 24);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -55,91 +74,253 @@
             this.panel1.Controls.Add(this.groupBox2);
             this.panel1.Controls.Add(this.groupBox1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panel1.Location = new System.Drawing.Point(720, 24);
+            this.panel1.Location = new System.Drawing.Point(472, 24);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(217, 408);
+            this.panel1.Size = new System.Drawing.Size(254, 408);
             this.panel1.TabIndex = 2;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.panel2);
+            this.groupBox2.Controls.Add(this.outputEcho);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox2.Location = new System.Drawing.Point(0, 118);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(254, 290);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Хз придумай как это";
             // 
             // panel2
             // 
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panel2.Location = new System.Drawing.Point(477, 24);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.rawInput);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(3, 18);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(243, 408);
-            this.panel2.TabIndex = 3;
+            this.panel2.Padding = new System.Windows.Forms.Padding(0, 3, 0, 0);
+            this.panel2.Size = new System.Drawing.Size(248, 26);
+            this.panel2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(154, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Отправить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // rawInput
+            // 
+            this.rawInput.Dock = System.Windows.Forms.DockStyle.Left;
+            this.rawInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.rawInput.Location = new System.Drawing.Point(0, 3);
+            this.rawInput.Name = "rawInput";
+            this.rawInput.Size = new System.Drawing.Size(154, 22);
+            this.rawInput.TabIndex = 0;
+            // 
+            // outputEcho
+            // 
+            this.outputEcho.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.outputEcho.Location = new System.Drawing.Point(3, 44);
+            this.outputEcho.Name = "outputEcho";
+            this.outputEcho.ReadOnly = true;
+            this.outputEcho.Size = new System.Drawing.Size(248, 243);
+            this.outputEcho.TabIndex = 0;
+            this.outputEcho.Text = "";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.panel7);
+            this.groupBox1.Controls.Add(this.panel6);
+            this.groupBox1.Controls.Add(this.panel5);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(254, 118);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Управление портом";
+            // 
+            // panel7
+            // 
+            this.panel7.Controls.Add(this.button3);
+            this.panel7.Controls.Add(this.button2);
+            this.panel7.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel7.Location = new System.Drawing.Point(3, 80);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(248, 35);
+            this.panel7.TabIndex = 2;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(129, 6);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(116, 23);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Закрыть порт";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(0, 6);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(116, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Открыть порт";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel6
+            // 
+            this.panel6.Controls.Add(this.label2);
+            this.panel6.Controls.Add(this.baudRate);
+            this.panel6.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel6.Location = new System.Drawing.Point(3, 49);
+            this.panel6.Name = "panel6";
+            this.panel6.Padding = new System.Windows.Forms.Padding(0, 3, 0, 0);
+            this.panel6.Size = new System.Drawing.Size(248, 31);
+            this.panel6.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(0, 3);
+            this.label2.Name = "label2";
+            this.label2.Padding = new System.Windows.Forms.Padding(6, 6, 0, 0);
+            this.label2.Size = new System.Drawing.Size(145, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Скорость передачи:";
+            // 
+            // baudRate
+            // 
+            this.baudRate.Dock = System.Windows.Forms.DockStyle.Right;
+            this.baudRate.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.baudRate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.baudRate.FormattingEnabled = true;
+            this.baudRate.Location = new System.Drawing.Point(159, 3);
+            this.baudRate.Name = "baudRate";
+            this.baudRate.Size = new System.Drawing.Size(89, 24);
+            this.baudRate.TabIndex = 0;
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.label1);
+            this.panel5.Controls.Add(this.portName);
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel5.Location = new System.Drawing.Point(3, 18);
+            this.panel5.Name = "panel5";
+            this.panel5.Padding = new System.Windows.Forms.Padding(0, 3, 0, 0);
+            this.panel5.Size = new System.Drawing.Size(248, 31);
+            this.panel5.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(0, 3);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(6, 6, 0, 0);
+            this.label1.Size = new System.Drawing.Size(50, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Порт:";
+            // 
+            // portName
+            // 
+            this.portName.Dock = System.Windows.Forms.DockStyle.Right;
+            this.portName.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.portName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.portName.FormattingEnabled = true;
+            this.portName.Location = new System.Drawing.Point(159, 3);
+            this.portName.Name = "portName";
+            this.portName.Size = new System.Drawing.Size(89, 24);
+            this.portName.TabIndex = 0;
             // 
             // panel3
             // 
+            this.panel3.Controls.Add(this.button4);
+            this.panel3.Controls.Add(this.label4);
             this.panel3.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panel3.Location = new System.Drawing.Point(249, 24);
+            this.panel3.Location = new System.Drawing.Point(244, 24);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(228, 408);
             this.panel3.TabIndex = 4;
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(75, 138);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "пуск";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(60, 69);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Пихай сюды вещи";
+            // 
             // panel4
             // 
+            this.panel4.Controls.Add(this.label3);
             this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel4.Location = new System.Drawing.Point(0, 24);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(249, 408);
+            this.panel4.Size = new System.Drawing.Size(244, 408);
             this.panel4.TabIndex = 5;
             // 
-            // groupBox1
+            // label3
             // 
-            this.groupBox1.Controls.Add(this.inputEcho);
-            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.groupBox1.Location = new System.Drawing.Point(0, 0);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(217, 201);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Исход";
-            // 
-            // groupBox2
-            // 
-            this.groupBox2.Controls.Add(this.outputEcho);
-            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.groupBox2.Location = new System.Drawing.Point(0, 201);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(217, 207);
-            this.groupBox2.TabIndex = 1;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Вход";
-            // 
-            // inputEcho
-            // 
-            this.inputEcho.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.inputEcho.Location = new System.Drawing.Point(3, 16);
-            this.inputEcho.Name = "inputEcho";
-            this.inputEcho.Size = new System.Drawing.Size(211, 182);
-            this.inputEcho.TabIndex = 0;
-            this.inputEcho.Text = "";
-            // 
-            // outputEcho
-            // 
-            this.outputEcho.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.outputEcho.Location = new System.Drawing.Point(3, 16);
-            this.outputEcho.Name = "outputEcho";
-            this.outputEcho.Size = new System.Drawing.Size(211, 188);
-            this.outputEcho.TabIndex = 0;
-            this.outputEcho.Text = "";
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(69, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Пихай сюды вещи";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(937, 432);
+            this.ClientSize = new System.Drawing.Size(726, 432);
             this.Controls.Add(this.panel4);
             this.Controls.Add(this.panel3);
-            this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Админка";
             this.panel1.ResumeLayout(false);
-            this.groupBox1.ResumeLayout(false);
             this.groupBox2.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.panel7.ResumeLayout(false);
+            this.panel6.ResumeLayout(false);
+            this.panel6.PerformLayout();
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -152,10 +333,23 @@
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.RichTextBox outputEcho;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.RichTextBox inputEcho;
-        private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox rawInput;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox baudRate;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox portName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- InterfaceStuff/Form1.cs
+++ InterfaceStuff/Form1.cs
@@ -8,33 +8,44 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO.Ports;
+using System.Threading;
 
 namespace InterfaceStuff
 {
     public partial class Form1 : Form
     {
         private SerialPort comport = new SerialPort();
+        private ManualResetEvent buffFull = new ManualResetEvent(false);
+        private byte[] dataBuffer;
+        private String[] baudRates = { "9600" };//хз надо ли другие. если попросит добавим
 
         public Form1()
         {
             InitializeComponent();
+            portName.Items.AddRange(SerialPort.GetPortNames());
+            portName.SelectedIndex = 0;//ну если ничего нету то вылетаем нафиг.вообще. потом поправлю кароч
+            baudRate.Items.AddRange(baudRates);
+            baudRate.SelectedIndex = 0;
             comport.DataReceived += new SerialDataReceivedEventHandler(port_DataRecived);
         }
 
         private void port_DataRecived(object sender, SerialDataReceivedEventArgs e)
         {
-            this.Invoke(new EventHandler(AddRecive));
+            dataBuffer = new byte[comport.BytesToRead];
+            comport.Read(dataBuffer, 0, dataBuffer.Length);
+            for (int i = 0; i < dataBuffer.Length; i++)
+                AddInputHistoryMessage(((char)dataBuffer[i]).ToString(), outputEcho);
+            comport.DiscardInBuffer();
+            buffFull.Set();//должно быть норм.хз чебудет если set на уже открытый эвент вызвать. так то у нас всегда есть while
         }
 
-        private void AddRecive(object sender, EventArgs e)
-        {
-            System.Threading.Thread.Sleep(100);
-            byte[] dataR = new byte[comport.BytesToRead];
-            comport.Read(dataR, 0, dataR.Length);
-            for (int i = 0; i < dataR.Length; i++)
-                AddInputHistoryMessage(((char)dataR[i]).ToString(), outputEcho);
-            AddInputHistoryMessage("\n", outputEcho);
-        }
+        //private void AddRecive(object sender, EventArgs e)
+        //{
+        //    System.Threading.Thread.Sleep(100);
+            
+            
+        //    AddInputHistoryMessage("\n", outputEcho);
+        //}
 
         private void AddInputHistoryMessage(string msg, RichTextBox richTextBox)
         {
@@ -48,37 +59,87 @@ namespace InterfaceStuff
             );
         }
 
-        private void sendData(string command)
+        private void writeToPort(String msg)
         {
-            comport.WriteLine(command + (char)0x0D);
-            AddInputHistoryMessage(command + "\n", inputEcho);
+            comport.WriteLine(msg + (char)0x0D);
+            AddInputHistoryMessage(msg + '\n', outputEcho);
+            comport.DiscardOutBuffer();
         }
 
         private void initPort()
         {
             if (comport.IsOpen)
             {
-                comport.Close();
-                System.Threading.Thread.Sleep(100);
+                AddInputHistoryMessage("Порт уже открыт\n", outputEcho);
             }
-            //comport.PortName = portNameTb.Text;
-            int baudRate = 9600;
-            //надо скорость контрлить?
-            comport.BaudRate = baudRate;
-            comport.DataBits = 8;
-            comport.StopBits = StopBits.One;
-            comport.Parity = Parity.None;
-            comport.Handshake = Handshake.None;
-            comport.ReceivedBytesThreshold = 8;
-            comport.WriteBufferSize = 20;
-            comport.ReadBufferSize = 20;
-            comport.ReadTimeout = -1;
-            comport.WriteTimeout = -1;
-            comport.DtrEnable = false;
-            comport.Open();
-            comport.RtsEnable = true;
-            System.Threading.Thread.Sleep(100);
-            //что нибудь про то что прот открыли
+            else
+            {
+                comport.PortName = portName.SelectedText;
+                comport.BaudRate = int.Parse(baudRate.SelectedText);
+                comport.DataBits = 8;
+                comport.StopBits = StopBits.One;
+                comport.Parity = Parity.None;
+                comport.Handshake = Handshake.None;
+                comport.ReceivedBytesThreshold = 8;
+                comport.WriteBufferSize = 20;
+                comport.ReadBufferSize = 20;
+                comport.ReadTimeout = -1;
+                comport.WriteTimeout = -1;
+                comport.DtrEnable = false;
+                comport.Open();
+                comport.RtsEnable = true;
+                AddInputHistoryMessage("Порт открыт\n", outputEcho);
+            }
+        }
+
+        private void closePort()
+        {
+            if (comport.IsOpen)
+            {
+                comport.DiscardOutBuffer();
+                comport.DiscardInBuffer();
+                comport.Close();
+                System.Threading.Thread.Sleep(100);//пишут что сон полезен для здорового общения с портами
+                AddInputHistoryMessage("Порт закрыт\n", outputEcho);
+            }
+            else
+            {
+                AddInputHistoryMessage("Порт не открыт\n", outputEcho);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string msg = rawInput.Text;
+            writeToPort(msg);
+            if (!buffFull.WaitOne(500))
+            {
+                AddInputHistoryMessage("ничего не пришло", outputEcho);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            initPort();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            closePort();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            launch();
+        }
+
+        private void launch()
+        {
+            //найти модули
+            //настроить АЦП и ЦАП
+            //посчитать параметры
+            //высылать и читать че придет анализируя че придет
+            //когда перелезем за границу сделать уровень и зажечь лампочку
         }
     }
 }
