1ae90550749edd6297cf0bc6c8ede2d573a2ba18
Alexaderrand101git <Sonicrider2@yandex.ru>
2018-11-22 04:35:30 +0400

    Initial commit

---
+++ InterfaceStuff/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace InterfaceStuff
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.inputEcho = new System.Windows.Forms.RichTextBox();
+            this.outputEcho = new System.Windows.Forms.RichTextBox();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(937, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.groupBox2);
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(720, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(217, 408);
+            this.panel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.Location = new System.Drawing.Point(477, 24);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(243, 408);
+            this.panel2.TabIndex = 3;
+            // 
+            // panel3
+            // 
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel3.Location = new System.Drawing.Point(249, 24);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(228, 408);
+            this.panel3.TabIndex = 4;
+            // 
+            // panel4
+            // 
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel4.Location = new System.Drawing.Point(0, 24);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(249, 408);
+            this.panel4.TabIndex = 5;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.inputEcho);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(217, 201);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Исход";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.outputEcho);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(0, 201);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(217, 207);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Вход";
+            // 
+            // inputEcho
+            // 
+            this.inputEcho.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.inputEcho.Location = new System.Drawing.Point(3, 16);
+            this.inputEcho.Name = "inputEcho";
+            this.inputEcho.Size = new System.Drawing.Size(211, 182);
+            this.inputEcho.TabIndex = 0;
+            this.inputEcho.Text = "";
+            // 
+            // outputEcho
+            // 
+            this.outputEcho.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.outputEcho.Location = new System.Drawing.Point(3, 16);
+            this.outputEcho.Name = "outputEcho";
+            this.outputEcho.Size = new System.Drawing.Size(211, 188);
+            this.outputEcho.TabIndex = 0;
+            this.outputEcho.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(937, 432);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Админка";
+            this.panel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RichTextBox outputEcho;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox inputEcho;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+    }
+}
+
---
+++ InterfaceStuff/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace InterfaceStuff
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort comport = new SerialPort();
+
+        public Form1()
+        {
+            InitializeComponent();
+            comport.DataReceived += new SerialDataReceivedEventHandler(port_DataRecived);
+        }
+
+        private void port_DataRecived(object sender, SerialDataReceivedEventArgs e)
+        {
+            this.Invoke(new EventHandler(AddRecive));
+        }
+
+        private void AddRecive(object sender, EventArgs e)
+        {
+            System.Threading.Thread.Sleep(100);
+            byte[] dataR = new byte[comport.BytesToRead];
+            comport.Read(dataR, 0, dataR.Length);
+            for (int i = 0; i < dataR.Length; i++)
+                AddInputHistoryMessage(((char)dataR[i]).ToString(), outputEcho);
+            AddInputHistoryMessage("\n", outputEcho);
+        }
+
+        private void AddInputHistoryMessage(string msg, RichTextBox richTextBox)
+        {
+            richTextBox.Invoke(
+                    new EventHandler(delegate
+                    {
+                        richTextBox.AppendText(msg);
+                        richTextBox.ScrollToCaret();
+                    }
+                )
+            );
+        }
+
+        private void sendData(string command)
+        {
+            comport.WriteLine(command + (char)0x0D);
+            AddInputHistoryMessage(command + "\n", inputEcho);
+        }
+
+        private void initPort()
+        {
+            if (comport.IsOpen)
+            {
+                comport.Close();
+                System.Threading.Thread.Sleep(100);
+            }
+            //comport.PortName = portNameTb.Text;
+            int baudRate = 9600;
+            //надо скорость контрлить?
+            comport.BaudRate = baudRate;
+            comport.DataBits = 8;
+            comport.StopBits = StopBits.One;
+            comport.Parity = Parity.None;
+            comport.Handshake = Handshake.None;
+            comport.ReceivedBytesThreshold = 8;
+            comport.WriteBufferSize = 20;
+            comport.ReadBufferSize = 20;
+            comport.ReadTimeout = -1;
+            comport.WriteTimeout = -1;
+            comport.DtrEnable = false;
+            comport.Open();
+            comport.RtsEnable = true;
+            System.Threading.Thread.Sleep(100);
+            //что нибудь про то что прот открыли
+        }
+    }
+}
